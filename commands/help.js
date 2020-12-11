7661e0452894187a13667c5d380b45970d8e8239
Djoofy-Main <jonas.mueleman@icloud.com>
2020-12-11 18:03:49 +0100

    Command info :airplane:

--- commands/help.js
+++ commands/help.js
@@ -14,32 +14,54 @@ module.exports = {
 		const { commands } = message.client;
         const member = message.mentions.members.first() || message.member;
         
-        // Create embed
-        const Embed = new Discord.MessageEmbed()
-            .attachFiles(["./files/icon.gif"])
-            .setDescription(`**__Djoofy's help command__**: \nsend **\`${this.usage}\`** to get info on a specific command`)
-            .setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor)
-            .setThumbnail("attachment://icon.gif")
-            .setTimestamp()
-            .setFooter("Made by FLODJES#5225")
+        if (!args.length) {
+			// inside a command, event listener, etc.
+            // Create embed
+            const Embed = new Discord.MessageEmbed()
+                .attachFiles(["./files/icon.gif"])
+                .setDescription(`**__Djoofy's help command__**: \nsend **\`${this.usage}\`** to get info on a specific command`)
+                .setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor)
+                .setThumbnail("attachment://icon.gif")
+                .setTimestamp()
+                .setFooter("Made by FLODJES#5225")
 
-            categories.forEach(cat => {
-				const value = []
-				commands.forEach(com => {
-					if (com.category == cat) {
-						value.push(`❯ **${prefix}${com.name}:** ${com.description} \`[${com.aliases}]\``);
-					}
-				});
-				if (value.length != 0) {
-					Embed.addField("__" + cat + "__", value);
-				}
-            });
-        const Embed2 = new Discord.MessageEmbed()
-
-        .addField("__Extra__", stripIndents`
-        ❯ [**Invite**](https://discord.com/api/oauth2/authorize?client_id=780154878395547670&permissions=8&scope=bot)
-        ❯ [**Vote**](https://discordbotlist.com/bots/djoofy)
-            `);
-        message.channel.send(Embed);
+                categories.forEach(cat => {
+                    const value = []
+                    commands.forEach(com => {
+                        if (com.category == cat) {
+                            value.push(`❯ **${prefix}${com.name}:** ${com.description} \`[${com.aliases}]\``);
+                        }
+                    });
+                    if (value.length != 0) {
+                        Embed.addField(stripIndents, "__" + cat + "__", value);
+                    }
+                });
+                return message.channel.send(Embed);
+            } else {
+                const name = args[0].toLowerCase();
+                const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+    
+                if (!command) {
+                    return message.reply("That's not a valid command!");
+                }
+    
+                let aliases = "None";
+                if (command.aliases) {
+                    aliases = command.aliases.join(", ");
+                }
+    
+                const Embed = new Discord.MessageEmbed()
+                    .setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor)
+                    .setTitle("Extra command info.")
+                    .setDescription(prefix + command.name + ":")
+                    .addFields(
+                        { name: "**__Description__**", value: command.description },
+                        { name: "**__Aliases__**", value: "`[" + aliases + "]`", inline: true },
+                    )
+                    .setTimestamp()
+                    .setFooter("Made by FLODJES#5225")    
+    
+                message.channel.send(Embed);
+            }
+        },
     }
-  }
