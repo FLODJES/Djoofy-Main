adbe975383b3196f0fe7837c21be0303071c946b
Djoofy-Main <jonas.mueleman@icloud.com>
2020-12-12 16:56:54 +0100

    :)))

--- commands/minecraft/server.js
+++ commands/minecraft/server.js
@@ -18,32 +18,37 @@ module.exports = {
         
         const serverIP = args[0] || 'play.hypixel.net';
 
-        // Log the server status
-        util.status(serverIP) // port is default 25565
-            .then((response) => {
-                // console.log(response); // For the response, dissabled for log feed.
-                var icon = response.favicon.replace("data:image/png;base64,", " ")
-                var base64str = icon;
-                var bitmap = new Buffer.alloc(16384, base64str.toString(), 'base64');
-                    fs.writeFileSync('./server-icon.png', bitmap);
+        try {
+            // Log the server status
+            util.status(serverIP) // port is default 25565
+                .then((response) => {
+                    // console.log(response); // For the response, dissabled for log feed.
+                    var icon = response.favicon.replace("data:image/png;base64,", " ")
+                    var base64str = icon;
+                    var bitmap = new Buffer.alloc(16384, base64str.toString(), 'base64');
+                        fs.writeFileSync('./server-icon.png', bitmap);
 
-                // Create embed
-                const Embed = new Discord.MessageEmbed()
-                    .setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor)
-                    .setTimestamp()
-                    .attachFiles('./server-icon.png')
-                    .setThumbnail('attachment://server-icon.png')
-                    .setTitle('__Server Status__')
-                    .addField('**❯ Server IP:**', response.host)
-                    .addField('**❯ Server Version:**', response.version)
-                    .addField('**❯ Online Players:**', response.onlinePlayers)
-                    .addField('**❯ Max Players:**', response.maxPlayers)
-                    .setFooter("Made by FLODJES#5225")
-                message.channel.send(Embed);
-            })
-            .catch((error) => {
-                throw error;
-            });
-
+                    // Create embed
+                    const Embed = new Discord.MessageEmbed()
+                        .setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor)
+                        .setTimestamp()
+                        .attachFiles('./server-icon.png')
+                        .setThumbnail('attachment://server-icon.png')
+                        .setTitle('__Server Status__')
+                        .addField('**❯ Server IP:**', response.host)
+                        .addField('**❯ Server Version:**', response.version)
+                        .addField('**❯ Online Players:**', response.onlinePlayers)
+                        .addField('**❯ Max Players:**', response.maxPlayers)
+                        .setFooter("Made by FLODJES#5225")
+                    message.channel.send(Embed);
+                })
+                .catch((error) => {
+                    throw error;
+                    
+                });
+        } catch(error) {
+            message.reply("That's an invalid server.");
+            console.log(error)
+        }
     }
-  }
+} 
