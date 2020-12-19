f0df011b21955794c7d2f99e1d6b71ee3ef0ecd5
Djoofy-Main <jonas.mueleman@icloud.com>
2020-12-19 18:02:04 +0100

    .

--- commands/minecraft/cape.js
+++ commands/minecraft/cape.js
@@ -2,8 +2,7 @@ const Discord = require("discord.js");
 const { stripIndents } = require("common-tags");
 const mcdata = require('mcdata');
 const config = require("../../config.json");
-const fs = require("fs");
-decode64 = require("base-64").decode;
+const Canvas = require('canvas');
 
 
 module.exports = {    
@@ -13,29 +12,37 @@ module.exports = {
     aliases: ["cape", "c"],
     usage: config["prefix"] + "cape [username]",
     category: config["categories"][0],
-    execute(message, args) {
+    execute : async (message, args) => {
         
         if(!args[0]) return message.channel.send("You must include a minecraft Java player's ign")
         const user = args[0];
+        const response = mcdata.playerStatus(user).then((response) => {
+            console.log(response);
+        });
 
-        mcdata.playerStatus(user).then((response) => {
-            // console.log(response);
+            const canvas = Canvas.createCanvas(210, 310);
+            const ctx = canvas.getContext('2d');
+
+
+            const capeImage = await Canvas.loadImage('./files/cape.png');
+            ctx.drawImage(capeImage, 0, 0, canvas.width, canvas.height);
+            // Use helpful Attachment class structure to process the file for you
+            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), './files/cape.png');
+
+            message.channel.send(attachment);
+
+    },
+}; 
 
                 // Create embed
-                const Embed = new Discord.MessageEmbed()
+                /* const Embed = new Discord.MessageEmbed()
                     .setTitle("Player Cape")
                     .addField("**__Username__**", "❯ `" + response.username + "`")
-                    .setImage(response.skin.cape)
+                    .setImage(attachment)
                     .setFooter("Made by FLODJES#5225")
                     .setTimestamp();
                     if (message.channel.type != "dm" ) {
                         const member = message.mentions.members.first() || message.member;
                         Embed.setColor(member.displayHexColor === "#000000" ? "#ffffff" : member.displayHexColor);
                     };
-                message.channel.send(Embed);
-        })
-
-
-
-    },
-}; 
+                message.channel.send(Embed);  */
